4b28a39c7b12b0d6e4f4a4cbd5e4edaa63c8ca5a
Hannes Reinecke <hare@suse.de>
2007-05-30 15:38:02 +0200

    Revamp module setup
    There are several ways of figuring out which modules to use.
    But most of them are inefficient.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- lib/mkinitrd/setup/50-modules.sh
+++ lib/mkinitrd/setup/50-modules.sh
@@ -47,18 +47,13 @@ resolve_modules() {
 	    oops 7 "Cannot determine dependencies of module $module." \
 		"Is modules.dep up to date?"
 	fi
-	echo "$module_list"
-    done \
-    | awk ' # filter duplicates: we must not reorder modules here!
-	NF == 0     { next }
-	$1 in seen  { next }
-		    { seen[$1]=1
-		      # drop module parameters here: modprobe in the initrd
-		      # will pick them up again.
-		      print $1
-		    }
-    '
-    rm -f $temp
+	for mod in $module_list ; do
+	    if ! $(echo $resolved_modules | grep -q $mod) ; then
+		resolved_modules="$resolved_modules $mod"
+	    fi
+	done
+    done
+    echo $resolved_modules
 }
 
 resolve_modalias() {
@@ -72,84 +67,56 @@ resolve_modalias() {
 
 # gather all the modules we are supposed to copy
 modules=
-for script in $INITRD_PATH/boot/*; do
+for script in $INITRD_PATH/boot/*.sh; do
     if use_script "$script"; then # only include the modules if the script gets used
 	verbose -n ""
 	for module in $(cat $script | egrep '%udevmodules: |%modules: ' | sed 's/^.*s: \(.*\)$/\1/'); do
 	    [ "$module" ] && verbose "[MODULES]\t$(basename $script): $(eval echo $module)"
-	    modules="$modules $(eval echo $module)"
+	    add_module $(eval echo $module)
 	done
     fi
 done
 
-echo -ne "Kernel Modules:\t"
-(
-shownmodules=
-for module in $modules; do
-    readmodule=${module##*/}
-    readmodule=${readmodule%*.ko}
-    show=1
-    for smodule in $shownmodules; do
-    	if [ "$smodule" = "$module" ]; then
-    	    show=
-    	fi
-    done
-    if [ "$show" ]; then
-	shownmodules="$shownmodules $readmodule"
-	
-    	if [ $readmodule != "${readmodule%:*}" ]; then
-    	    resolved_modalias="$(resolve_modalias $readmodule)"
-	    shownmodules="$shownmodules $resolved_modalias"
-	    echo -n "$readmodule("
-	    COMMA=
-	    for modalias in $resolved_modalias; do
-		echo -n ${COMMA}${modalias}
-		[ ! "$COMMA" ] && COMMA=,
-    	    done
-	    echo ")"
-    	else
-	    echo "$readmodule "
-        fi
-    fi
-done
-) | sort | uniq | (while read a; do echo -n "$a "; done) # print a sorted module list (easier to find modules)
-echo
-
 resolved_modules="$(resolve_modules $kernel_version $modules)"
 
-#echo "modules: $modules"
-#echo "resolved modules: $resolved_modules"
+echo -ne "Kernel Modules:\t"
+for mod in $resolved_modules ; do
+    modname=${mod##*/}
+    echo -n "${modname%%.ko} "
+done
+echo
 
-    # Copy all modules into the initrd
-    for module in $resolved_modules; do
-#	echo "copy module $module"
-        if [ ! -r $root_dir/$module ]; then
-            oops 9 "Module $module not found."
-            continue
-        fi
-        if ! ( cd ${root_dir:-/} ; cp -p --parents $module $tmp_mnt ) ; then
-            oops 6 "Failed to add module $module."
-            rm -rf $tmp_mnt
-            return
-        fi
-    done
+# Copy all modules into the initrd
+for module in $resolved_modules; do
+    if [ ! -r $root_dir/$module ]; then
+	oops 9 "Module $module not found."
+	continue
+    fi
+    if ! ( cd ${root_dir:-/} ; cp -p --parents $module $tmp_mnt ) ; then
+	oops 6 "Failed to add module $module."
+	rm -rf $tmp_mnt
+	return 1
+    fi
+done
 
-    # And run depmod to ensure proper loading
-    if [ "$sysmap" ] ; then
-        map="$sysmap"
-    else
-        map=$root_dir/boot/System.map-$kernel_version
-    fi
-    if [ ! -f $map ]; then
-        map=$root_dir/boot/System.map
-    fi
-    if [ ! -f $map ]; then
-        oops 9 "Could not find map $map, please specify a correct file with -M."
-        rm -rf $tmp_mnt
-        return
-    fi
+# And run depmod to ensure proper loading
+if [ "$sysmap" ] ; then
+    map="$sysmap"
+else
+    map=$root_dir/boot/System.map-$kernel_version
+fi
+if [ ! -f $map ]; then
+    map=$root_dir/boot/System.map
+fi
+if [ ! -f $map ]; then
+    oops 9 "Could not find map $map, please specify a correct file with -M."
+    rm -rf $tmp_mnt
+    return 1
+fi
 
-    [ -d $tmp_mnt/lib/modules/$kernel_version ] || mkdir -p $tmp_mnt/lib/modules/$kernel_version
+if [ "$resolved_modules" ] ; then
+    [ ! -d $tmp_mnt/lib/modules/$kernel_version ] && oops 10 "No modules have been installed"
     ( cd $tmp_mnt; /sbin/depmod -b $tmp_mnt -e -F $map $kernel_version )
+fi
 
 
