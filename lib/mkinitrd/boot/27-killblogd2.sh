433b6e311ff5d0aa511ba350f248145cc1dff280
Alexander Graf <agraf@linux-m0u6.suse.de>
2007-06-20 14:13:41 +0200

    fixed waiting for blogd properly now

--- lib/mkinitrd/boot/27-killblogd2.sh
+++ lib/mkinitrd/boot/27-killblogd2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#%programs: usleep
 #%dontshow
 #
 ##### blogd end2
@@ -11,6 +12,8 @@
 
 if test -n "$blogd_pid" ; then
     kill -QUIT "$blogd_pid"
-    wait "$blogd_pid"
+    while [ -d "/proc/$blogd_pid" ]; do
+	usleep 300000
+    done
     umount -t devpts /root/dev/pts
 fi
