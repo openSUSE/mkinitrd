02a6bc24c53a7c73c95e39ae86d962899ec62d74
Alexander Graf <agraf@linux-m0u6.suse.de>
2007-07-06 14:27:52 +0200

    removed dependency on grep (use sed instead)

--- scripts/boot-start.sh
+++ scripts/boot-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #%stage: boot
-#%programs: bash umount test mount mknod mkdir ln /sbin/blogd date sleep echo cat /bin/sed /sbin/insmod /sbin/modprobe expr kill /sbin/killall5 /sbin/halt /sbin/reboot /sbin/showconsole cp /sbin/pidof mv chmod rm true /bin/grep ls /lib/mkinitrd/bin/*
+#%programs: bash umount test mount mknod mkdir ln /sbin/blogd date sleep echo cat /bin/sed /sbin/insmod /sbin/modprobe expr kill /sbin/killall5 /sbin/halt /sbin/reboot /sbin/showconsole cp /sbin/pidof mv chmod rm true ls /lib/mkinitrd/bin/*
 #%modules: $RESOLVED_INITRD_MODULES
 #%dontshow
 #
