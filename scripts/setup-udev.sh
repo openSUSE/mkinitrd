b3a30108a505717c88b483f3bac7e67f91280d08
Hannes Reinecke <hare@suse.de>
2007-07-17 15:45:45 +0200

    setup-udev: Update for latest udev changes.

--- scripts/setup-udev.sh
+++ scripts/setup-udev.sh
@@ -4,22 +4,22 @@
 #%depends: start
 # 
 mkdir -p $tmp_mnt/etc/udev/rules.d
-# Create our own udev.conf
-echo "udev_root=\"/dev\"" > $tmp_mnt/etc/udev/udev.conf
-echo "udev_rules=\"/etc/udev/rules.d\"" >> $tmp_mnt/etc/udev/udev.conf
 # copy needed rules
-for rule in 05-udev-early.rules 50-udev-default.rules 59-dasd.rules 60-persistent-storage.rules 64-device-mapper.rules; do
+for rule in \
+    05-udev-early.rules \
+    50-udev-default.rules \
+    59-dasd.rules \
+    60-persistent-storage.rules \
+    60-persistent-input.rules \
+    64-device-mapper.rules \
+    64-md-raid.rules \
+    80-drivers.rules; do
     if [ -f /etc/udev/rules.d/$rule ]; then
 	cp /etc/udev/rules.d/$rule $tmp_mnt/etc/udev/rules.d
     fi
 done
-# include module autoloading
-echo 'ACTION=="add", ENV{MODALIAS}=="?*", RUN+="/sbin/modprobe $env{MODALIAS}"' >> ./etc/udev/rules.d/50-udev-default.rules
-# copy helper scripts
+# copy helper
 mkdir -p $tmp_mnt/lib/udev
-if [ -f /sbin/vol_id ] ; then
-    ln -s ../../sbin/vol_id ${tmp_mnt}/lib/udev/vol_id
-fi
 for script in /lib/udev/* /sbin/*_id ; do
     if [ ! -d "$script" ] && [ -x "$script" ] ; then
 	cp_bin $script ${tmp_mnt}${script}
