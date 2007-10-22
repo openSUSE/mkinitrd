89a606bc947cd88789d2a818d1755fd70ddd2ef8
Hannes Reinecke <hare@suse.de>
2007-10-22 13:57:03 +0200

    Use generic rule to determine block partitions
    We can just use the generic rule to determine block partitions:
    If it is listed in /proc/partitions, it's a partitionable device
    or a partition. The partition delimiter is 'p' if the disk name
    ends with a number, otherwise the numbers follows the last letter
    is the partition number.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- scripts/setup-blockpartition.sh
+++ scripts/setup-blockpartition.sh
@@ -7,17 +7,12 @@ blockpart_blockdev=
 
 for bd in $blockdev; do
 	update_blockdev $bd
-	if [ "$(echo $bd | egrep '[a-z][0-9]*$')" ]; then
-	    if [ "${blockdriver%%[0-9]*}" = "cciss" ] ; then
-		blkpart="cciss!${bd##*/}"
-		blkdev="${blkpart%%p[0-9]}"
-	    elif [ "${blockdriver%%[0-9]*}" = "ida" ] ; then
-		blkpart="ida!${bd##*/}"
-		blkdev="${blkpart%%p[0-9]}"
-	    else
-		blkpart="${bd##*/}"
-		blkdev="$(echo $blkpart | sed 's/^\([a-z|!]*\)[0-9]*$/\1/')"
-	    fi
+	blkpart=$(majorminor2blockdev $blockmajor $blockminor)
+	# /proc/partitions lists only partitions or devices which are
+	# are partitionable
+	if [ "$blkpart" ]; then
+	    blkdev=$(echo ${blkpart#/dev/} | sed 's./.!.g')
+	    blkdev=$(echo $blkpart | sed 's/\([a-z]\)[0-9]*$/\1/;s/p$//')
 	    if [ -d /sys/block/$blkdev/$blkpart ] ; then
 		blkdev=$(echo $blkdev | sed 's.!./.g')
 		bd="/dev/$blkdev"
