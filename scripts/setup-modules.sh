d229eac57c5d7adf839bffb26998d78a105ad686
Bernhard Walle <bwalle@suse.de>
2008-12-01 21:38:00 +0100

    Add handling for unsupported kernels
    mkinitrd doesn't add kernel modules which are unsupported. However, that does
    not make sense when you have a non-SUSE kernels because it prevents users from
    using a non-SUSE kernel on SLES at all without changing
    /etc/modprobe.d/unsupported-modules.
    
    This patch checks if the 'ext3.ko' (because it's always present) module
    is unsupported. If it is, then we assume that the kernel is unsupported as a
    whole. Then we add that kernel modules to initrd.
    
    Because the patch in module-init-tools checks if we're running on a unsupported
    kernel and loads the kernel modules then, that initrd is actually usable.
    
    Signed-off-by: Bernhard Walle <bwalle@suse.de>

--- scripts/setup-modules.sh
+++ scripts/setup-modules.sh
@@ -29,11 +29,71 @@ add_module() {
     done
 }
 
+# Brief
+#       Checks if the kernel version is supported at all.
+#
+# Description
+#       Checks if the kernel version is supported. The background is: If we
+#       have a kernel that is completely unsupported, we want to include all
+#       modules even if the modules don't have the 'supported' attribute.
+#       That allows us to use self-made kernels even on SUSE LINUX Enterprise.
+#       The supported flag should not prevent users from running self-compiled
+#       kernels on SLES but should prevent the loading of unsupported modules
+#       on a supported kernel. Because the modprobe command is already aware
+#       if it runs on a system that doesn't have the SUSE supported patch in
+#       the kernel and then just loads the kernel module, we can safely include
+#       such modules on such systems in initrd.
+#
+#       The check is done by checking if 'ext3.ko' is supported. Since we
+#       have to supported ext3 for a very long period of time from now because
+#       it's our standard file system, and that module exists on every
+#       architecture and does not depend on the hardware and even exists in
+#       the base package of the kernel because we need it in a virtualised
+#       environment, that module is suitable for that check. I didn't know any
+#       better method of checking if the kernel is supported so I implemented
+#       that hack. Better than nothing. :-)
+#
+# Parameters
+#       kernelver: the kernel version
+#
+# Return value
+#       0 (true) if the kernel is supported, 1 (false) if the kernel is not
+#       supported
+check_supported_kernel() {
+    local kernel_version=$1
+    local output=
+
+    output=$(modinfo -k "$kernel_version" -F supported 2>/dev/null)
+    if [ "$?" -ne 0 ] ; then
+        # If the command existed with an error, assume that the kernel is
+        # supported. That is just the same behaviour as before we did that
+        # check_supported_kernel() hack
+        verbose "[MODULES]\t'modinfo -k \"$kernel_version\" -F supported' " \
+                "returned with an error."
+        return 0
+    fi
+
+    if [[ "$output" = *yes* ]] ; then
+        verbose "[MODULES]\tSupported kernel ($kernel_version)"
+        return 0
+    else
+        verbose "[MODULES]\tUnsupported kernel ($kernel_version)"
+        return 1
+    fi
+}
+
 # Resolve module dependencies and parameters. Returns a list of modules and
 # their parameters.
 resolve_modules() {
-    local kernel_version=$1 module
+    local kernel_version=$1
+    local module=
+    local supported=
+    local additional_args=
     shift
+
+    if ! check_supported_kernel $kernel_version ; then
+        additional_args=--allow-unsupported-modules
+    fi
 
     for module in "$@"; do
 	local with_modprobe_conf
@@ -53,6 +113,7 @@ resolve_modules() {
 	module_list=$(/sbin/modprobe $with_modprobe_conf \
 	    --set-version $kernel_version --ignore-install \
 	    --show-depends $module \
+            $additional_args \
 	    | sed -ne 's:.*insmod /\?::p' | sed -ne 's:\ .*\?::p' )
 	if [ ! "$module_list" ]; then
 	    echo \
