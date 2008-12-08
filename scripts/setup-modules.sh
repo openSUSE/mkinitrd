openSUSE/mkinitrd

commit 9e82cd561d229469395b8f46e0322ba9ba24fb0d

Include virtio_pci if necessary
When using virtio_net as network device, we also need virtio_pci (and its
dependency virtio_ring) in the initrd. The dependency isn't expressed in
udev rules or install-lines (/etc/modprobe.conf*). The virtio_pci driver is just
loaded by the modalias

    pci:v00001AF4d*sv*sd*bc*sc*i*

in the module. Therefore we have to hardcode that dependency, at least for now.

References: bnc #456439

Signed-off-by: Bernhard Walle <bwalle@suse.de>

scripts/setup-modules.sh
@@ -8,7 +8,11 @@
 # Global variables
 # Array that stores additional dependencies. Each entry looks like
 #       module:module1 module2
-declare -a additional_module_dependencies
+# The array is initialised with some known dependency and extended at runtime
+# in the load_additional_dependencies function.
+additional_module_dependencies=(
+        "virtio:virtio_pci virtio_ring"
+)
 
 # Check if module $1 is listed in $modules.
 has_module() {

commit af613526259aaaef9c53eb7818f8181d2494563a

Actually execute load_additional_dependencies
Commit 9e82cd561d229469395b8f46e0322ba9ba24fb0d broke the execution of
load_additional_dependencies because we checked if the global
additional_module_dependencies array contained data. Because that commit
pre-filled additional_module_dependencies with data, the function was
never executed.

Signed-off-by: Bernhard Walle <bwalle@suse.de>

scripts/setup-modules.sh
@@ -107,11 +107,6 @@ check_supported_kernel() {
 #       additional_module_dependencies array.
 load_additional_dependencies()
 {
-    # array already filled
-    if [ ${#additional_module_dependencies[*]} -ne 0 ] ; then
-        return
-    fi
-
     for file in /etc/modprobe.conf \
                 /etc/modprobe.conf.local \
                 /etc/modprobe.d/* ; do