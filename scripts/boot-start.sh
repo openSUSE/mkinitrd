openSUSE/mkinitrd

commit 4b0eb6f67d6c8e258cf27efa90c5d466c88f5e08

Don't load drivers via modprobe that require firmware
Drivers listed in INITRD_MODULES in /etc/sysconfig/kernel are loaded before
udev is started via modprobe. However, udev is required to load firmware files.
The fix is to exclude drivers that require firmware files from being loaded
via INITRD_MODULES. The resolution is done when creating the initrd, not at boot
time.

References: bnc #457428
Signed-off-by: Bernhard Walle <bwalle@suse.de>

scripts/setup-start.sh
@@ -55,4 +55,18 @@ if is_xen_kernel $kernel_version; then
 else
     RESOLVED_INITRD_MODULES="$modules"
 fi
+
+# Drivers with a firmware cannot be loaded via INITRD_MODULES mechanism
+# because udev is needed to load the firmware. Put them in initrd, but don't
+# load them via modprobe.
+RESOLVED_INITRD_MODULES_NEW=
+for m in $RESOLVED_INITRD_MODULES ; do
+    if [ $(modinfo -F firmware $m 2> /dev/null | wc -l) -gt 0 ] ; then
+        verbose "[SETUP]\tExcluding $m from INITRD_MODULES because of firmware"
+    else
+        RESOLVED_INITRD_MODULES_NEW="$RESOLVED_INITRD_MODULES_NEW $m"
+    fi
+done
+RESOLVED_INITRD_MODULES=$RESOLVED_INITRD_MODULES_NEW
+
 save_var RESOLVED_INITRD_MODULES

commit a7be75e000ca652f4101c3ac5f2ed4fa7f733ff4

Fix exclusion of firmware modules from INITRD_MODULES
Commit 4b0eb6f67d6c8e258cf27efa90c5d466c88f5e08 introduced the problem that now
modules that require a firmware are not included into initrd at all if only
listed via INITRD_MODULES and not detected automatically. Now we include such
modules via the %udevmodules comment but don't load it manually with 'modprobe'
at system boot (i.e., via. %modules). That should work for that case.

References: bnc #457428
Signed-off-by: Bernhard Walle <bwalle@suse.de>

scripts/boot-start.sh
@@ -4,6 +4,7 @@
 #%depends: devfunctions
 #%programs: /bin/bash umount test mount mknod mkdir ln date sleep echo cat /bin/sed /sbin/insmod /sbin/modprobe kill /sbin/killall5 /sbin/halt /sbin/reboot /sbin/showconsole cp /sbin/pidof mv chmod rm true ls /lib/mkinitrd/bin/*
 #%modules: $RESOLVED_INITRD_MODULES
+#%udevmodules: $RESOLVED_INITRD_MODULES_UDEV
 #%dontshow
 #
 ##### Initrd initialization

scripts/setup-start.sh
@@ -59,10 +59,13 @@ fi
 # Drivers with a firmware cannot be loaded via INITRD_MODULES mechanism
 # because udev is needed to load the firmware. Put them in initrd, but don't
 # load them via modprobe.
+RESOLVED_INITRD_MODULES_UDEV=
 RESOLVED_INITRD_MODULES_NEW=
 for m in $RESOLVED_INITRD_MODULES ; do
     if [ $(modinfo -F firmware $m 2> /dev/null | wc -l) -gt 0 ] ; then
-        verbose "[SETUP]\tExcluding $m from INITRD_MODULES because of firmware"
+        verbose "[SETUP]\tDon't load $m on boot with modprobe because "\
+                "it requires firmware"
+        RESOLVED_INITRD_MODULES_UDEV="$RESOLVED_INITRD_MODULES_UDEV $m"
     else
         RESOLVED_INITRD_MODULES_NEW="$RESOLVED_INITRD_MODULES_NEW $m"
     fi
@@ -70,3 +73,4 @@ done
 RESOLVED_INITRD_MODULES=$RESOLVED_INITRD_MODULES_NEW
 
 save_var RESOLVED_INITRD_MODULES
+save_var RESOLVED_INITRD_MODULES_UDEV