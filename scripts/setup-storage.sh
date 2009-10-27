65adb2755ca88c7fe0c08770b5fef7f284f9fb4c
Milan Vancura <mvancura@suse.cz>
2009-10-27 15:02:37 +0100

    Allow root=LABEL= syntax back again
    Fix commit a05c3884:
    setup-storage.sh:resolve_device(): don't die so young!
    
    References: bnc#483136

--- scripts/setup-storage.sh
+++ scripts/setup-storage.sh
@@ -194,11 +194,6 @@ resolve_device() {
     local x="$2"
     local realrootdev="$2"
 
-    # root device was already checked and non-existing
-    # non-root device is not fatal, but may not be
-    # shown to the following block resolver modules
-    [ -b "$realrootdev" ] || exit 0
-
     case "$realrootdev" in
       LABEL=*|UUID=*)
         # get real root via fsck hack
@@ -217,6 +212,12 @@ resolve_device() {
       *:*|//*)
         [ "$type" = "Root" ] && x="$rootfstype-root"
         ;;
+      *)
+	# root device was already checked and non-existing
+	# non-root device is not fatal, but may not be
+	# shown to the following block resolver modules
+	[ -b "$realrootdev" ] || exit 0
+	;;
     esac
 
     [ "$x" != "$realrootdev" ] && x="$x ($realrootdev)"
