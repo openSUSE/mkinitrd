8420f1ce7e96757f435daaa895a833b914a866be
Milan Vancura <mvancura@suse.cz>
2009-12-14 14:28:54 +0100

    Don't limit inodes in /dev
    References: bnc#529839

--- scripts/boot-start.sh
+++ scripts/boot-start.sh
@@ -33,7 +33,7 @@ die() {
 
 mount -t proc  proc  /proc
 mount -t sysfs sysfs /sys
-mount -t tmpfs -o mode=0755 udev /dev
+mount -t tmpfs -o mode=0755,nr_inodes=0 udev /dev
 
 mknod -m 0666 /dev/tty     c 5 0
 mknod -m 0600 /dev/console c 5 1
