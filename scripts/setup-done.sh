0ab37f5504faf4c28787be13772e33e01cce1540
Michal Marek <mmarek@suse.cz>
2010-02-04 22:29:05 +0100

    Handle low space below /dev/shm better
    Pick first directory among $TMPDIR, /dev/shm, /tmp and /var/tmp that has
    at least 50MB free space. If all have below 50MB, pick the one with most
    free space and issue a warning. Also abort if creating the cpio image
    fails.
    
    References: bnc#553629
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/setup-done.sh
+++ scripts/setup-done.sh
@@ -6,8 +6,11 @@
 
 pushd . > /dev/null 2>&1
 cd $tmp_mnt
-find bin sbin -type f -print0 | xargs -0 chmod 0755 
-find . ! -name "*~" | cpio -H newc --create | gzip -9 > $tmp_initrd.gz
+find bin sbin -type f -print0 | xargs -0 chmod 0755
+if ! find . ! -name "*~" | cpio -H newc --create | gzip -9 > $tmp_initrd.gz
+then
+    oops 8 "Failed to build initrd"
+fi
 popd > /dev/null 2>&1
 if ! cp -f $tmp_initrd.gz $initrd_image ; then
     oops 8 "Failed to install initrd"
