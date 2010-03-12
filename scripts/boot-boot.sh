5346ba47f54e7bfef099852bfa5b1ff3b31ccd61
Stephan Kulow <coolo@suse.de>
2010-03-12 12:07:11 +0100

    support upstart in providing a file with the initrd exports
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/boot-boot.sh
+++ scripts/boot-boot.sh
@@ -29,6 +29,14 @@ unset check_for_device
 ROOTFS_BLKDEV="$rootdev"
 export ROOTFS_BLKDEV
 
+# upstart does not export the full environment to jobs by default
+# and we want our boot.* scripts to know the initrd environment for
+# quite a while
+# technically we only need the variables exported by initrd scripts
+# and the cmdline and we'll also get thinks like HOME and PWD, but
+# those are overwritten anyway
+export > /root/dev/shm/initrd_exports.sh
+
 exec /bin/run-init -c ./dev/console /root $init ${kernel_cmdline[@]}
 echo could not exec run-init!
 die 0
