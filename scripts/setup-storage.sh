0bc1e72a49d7eceb91accf8edf1fb0af08e8290c
Michal Marek <mmarek@suse.cz>
2010-05-10 13:21:55 +0200

    setup-storage.sh: Fix beautify_blockdev()
    References: bnc#604150
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/setup-storage.sh
+++ scripts/setup-storage.sh
@@ -70,6 +70,12 @@ majorminor2blockdev() {
 beautify_blockdev() {
         local olddev="$1" udevdevs dev
         
+        case "$olddev" in
+        /dev/md*)
+                # setup-md.sh doesn't understand the md-uuid-* symlinks
+                echo "$olddev"
+                return
+        esac
         # search for udev information
         udevdevs=$(/sbin/udevadm info -q symlink --name=$olddev)
         #   look up ata device links
@@ -88,8 +94,14 @@ beautify_blockdev() {
         done
         #   take the first guess
         for dev in $udevdevs; do
-                echo "/dev/$dev"
-                return
+                case "$dev" in
+                block/* | root)
+                        continue
+                        ;;
+                *)
+                        echo "/dev/$dev"
+                        return
+                esac
         done
         
         # get pretty name from device-mapper
