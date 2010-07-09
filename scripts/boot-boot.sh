c8e9fff6db2fc421b649dded9d3060de1637df26
Michal Marek <mmarek@suse.cz>
2010-07-09 22:50:10 +0200

    Load SELinux policy during boot if enabled
    Backported from Factory (commits f8c8a3e and b485c6c).
    
    References: bnc#598567
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/boot-boot.sh
+++ scripts/boot-boot.sh
@@ -14,9 +14,76 @@
 ## -----------------------
 ##
 
+
+# based on rd_load_policy() from fedora 12 dracut package
+selinux_load_policy()
+{
+	local NEWROOT=$1
+	local CR="${NEWROOT}/usr/bin/chroot"
+
+	local SELINUX="enforcing"
+	[ -e "$NEWROOT/etc/selinux/config" ] && . "$NEWROOT/etc/selinux/config"
+
+	# If SELinux is disabled exit now
+	if [ -z $cmd_selinux ]; then
+		return 0
+	fi
+	if [ -z $cmd_enforcing ]; then
+		return 0
+	fi
+	if [ $cmd_selinux == 0 ]; then
+		return 0
+	fi
+
+	# Check whether SELinux is in permissive mode
+	local permissive=0
+	if [ $cmd_enforcing == 0 ]; then
+		permissive=1
+	fi
+
+	# Attempt to load SELinux Policy
+	if [ -x "$NEWROOT/usr/sbin/load_policy" -o -x "$NEWROOT/sbin/load_policy" ]; then
+		local ret=0
+		echo "Loading SELinux policy"
+		# load_policy does mount /proc and /selinux in
+		# libselinux,selinux_init_load_policy()
+		if [ -x "$NEWROOT/sbin/load_policy" ]; then
+			$CR "$NEWROOT" /sbin/load_policy -i
+			ret=$?
+		else
+			$CR "$NEWROOT" /usr/sbin/load_policy -i
+			ret=$?
+		fi
+
+		if [ $ret -eq 0 -o $ret -eq 2 ]; then
+			return 0
+		fi
+
+		echo "Initial SELinux policy load failed."
+		if [ $ret -eq 3 -o $permissive -eq 0 ]; then
+			echo "Machine in enforcing mode."
+			echo "Not continuing"
+			sleep 100d # XXX well...
+			exit 1
+		fi
+		return 0
+	elif [ $permissive -eq 0 ]; then
+		echo "Machine in enforcing mode and cannot execute load_policy."
+		echo "To disable selinux, add selinux=0 to the kernel command line."
+		echo "Not continuing"
+		sleep 100d
+		exit 1
+	fi
+}
+
+
 # Move device nodes
 /bin/mount --move /dev /root/dev
 /bin/mount -t proc proc /root/proc
+
+# SELinux load policy
+selinux_load_policy "/root"
+
 # ready to leave
 cd /root
 umount -l /proc
