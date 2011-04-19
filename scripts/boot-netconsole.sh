129db081bd04071dde34dbab540b1c56fd9238f5
Egbert Eich <eich@freedesktop.org>
2011-04-19 13:59:26 +0200

    [netconsole]: Add deadline to initial ping command.
    So far 'single shot' ping was sent to the destination address using
    the '-c1' option. This timed out after two RTT (default for ping).
    If the network interface was not quite ready when ping tried to send
    the packet it was waiting for a reply that never came - with a timeout
    that's quite high for a local network.
    Giving ping a deadline, it will send packages until the deadline is
    exceeded or until the number of reply packages as specified with the
    '-c' option have been received.
    This results in a more reliable operation in this context and a shorter
    timeout if the other end doesn't respond.

--- scripts/boot-netconsole.sh
+++ scripts/boot-netconsole.sh
@@ -23,7 +23,7 @@ if [ "$netc_loghost" = "$netc_udpport" ]; then
         # no colon in NETCONSOLE => no port specified => use default
         netc_udpport="514"      # syslog
 fi
-ping -c1 $netc_loghost >/dev/null 2>&1
+ping -w5 -c1 $netc_loghost >/dev/null 2>&1
 netc_lladdr=$(arp $netc_loghost | while read ip type mac o; do [ "$mac" != "HWaddress" ] && echo $mac; done)
 netc_ipaddr=$(arp -n | while read ip type mac o; do [ "$mac" == "$netc_lladdr" ] && echo $ip; done)
 echo -e "Netconsole:\tlog to $netc_loghost:$netc_udpport [ $netc_ipaddr / $netc_lladdr ] via $interface"
