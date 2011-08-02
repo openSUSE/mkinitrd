4e7d94035c1880010b589b8183161e3cae350665
Michal Marek <mmarek@suse.cz>
2011-08-02 14:12:48 +0200

    Run cpio with the --quiet option
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/setup-done.sh
+++ scripts/setup-done.sh
@@ -7,7 +7,7 @@
 pushd . > /dev/null 2>&1
 cd $tmp_mnt
 find bin sbin -type f -print0 | xargs -0 chmod 0755
-if ! find . ! -name "*~" | cpio -H newc --create | gzip -9 > $tmp_initrd.gz
+if ! find . ! -name "*~" | cpio --quiet -H newc --create | gzip -9 > $tmp_initrd.gz
 then
     oops 8 "Failed to build initrd"
 fi
