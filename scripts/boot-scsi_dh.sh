b758d52be0b94e467c7c58f0f789573b1333bd24
Michal Marek <mmarek@suse.cz>
2012-01-20 17:18:51 +0100

    Load all scsi_dh_* modules early during boot
    Referenes: bnc#727428
    Suggested-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

---
+++ scripts/boot-scsi_dh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#%stage: boot
+#%depends: start
+#%modules: $scsi_dh_modules
+#%dontshow
+#%if: -n "$scsi_dh_modules"
---
+++ scripts/setup-scsi_dh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#%stage: device
+#
+# Include all scsi_dh_* modules and load them on boot (bnc#727428 et al)
+
+scsi_dh_modules=
+for i in $(find $root_dir/lib/modules/$kernel_version/kernel/drivers/scsi/device_handler -name "scsi[-_]dh[_-]*.ko"); do
+	i=${i%.ko}
+	scsi_dh_modules="$scsi_dh_modules ${i##*/}"
+done
+
+save_var scsi_dh_modules
