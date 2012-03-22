8355073e0972bfbc88dd3fc32f79bc53bd1ef249
Stefan Seyfried <seife+obs@b1-systems.com>
2012-03-22 10:59:13 +0100

    boot-udev: add blkid dependency
    udev really needs blkid and it is not copied correctly after the usr
    move. Adding it as a dependency to boot-udev.sh is a hack but makes
    it work again.

--- scripts/boot-udev.sh
+++ scripts/boot-udev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #%stage: boot
 #%depends: start dm
-#%programs: /sbin/udevd /sbin/udevadm /bin/uname
+#%programs: /sbin/udevd /sbin/udevadm /bin/uname /sbin/blkid
 #%dontshow
 #
 ##### udev initialization
