67dd510b2ffe027398b4067db146aabbe9081afe
Alexander Graf <agraf@suse.de>
2013-02-26 19:34:18 +0100

    Add support for zImage on ARM
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- scripts/setup-done.sh
+++ scripts/setup-done.sh
@@ -20,8 +20,8 @@ then
     oops 8 "Failed to build initrd"
 fi
 
-arch=$(uname -i)
-if [[ $arch =~ arm ]];then
+# uImages want to have uInitrds
+if [[ $kernel_image =~ uImage ]]; then
    mkimage -A arm -O linux -T ramdisk -C none -a 0x0 -e 0x0 \
            -n 'Initrd' -d  $tmp_initrd $tmp_initrd.uboot
    rm -f $tmp_initrd
