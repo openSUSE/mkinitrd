04e67a4f2f4215f4a4b90c6dfeaf0c7e0e704a66
Michal Marek <mmarek@suse.cz>
2013-06-05 21:17:31 +0200

    Add hid modules that are no longer builtin

--- scripts/boot-usb.sh
+++ scripts/boot-usb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #%stage: device
-#%udevmodules: usbcore ohci_hcd uhci-hcd ehci_hcd xhci-hcd usbhid hid-logitech-dj hid-generic
+#%udevmodules: usbcore ohci_hcd uhci-hcd ehci_hcd xhci-hcd usbhid hid-logitech-dj hid-generic hid-holtek-kbd hid-lenovo-tpkbd hid-logitech-dj hid-ortek hid-roccat-arvo hid-roccat-isku hid-samsung hid-apple hid-belkin hid-cherry hid-ezkey hid-microsoft
 #%if: "$use_usb"
 #
 ##### usb module helper
