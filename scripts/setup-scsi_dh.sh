08e5b188661565561de08ab9a5e94345da622e6f
Olaf Hering <olaf@aepfle.de>
2013-09-22 19:23:51 +0200

    Check if kver/kernel/drivers/scsi/device_handler/ exists

--- scripts/setup-scsi_dh.sh
+++ scripts/setup-scsi_dh.sh
@@ -5,9 +5,13 @@
 # Include all scsi_dh_* modules and load them on boot (bnc#727428 et al)
 
 scsi_dh_modules=
-for i in $(find $root_dir/lib/modules/$kernel_version/kernel/drivers/scsi/device_handler -name "scsi[-_]dh[_-]*.ko"); do
-	i=${i%.ko}
-	scsi_dh_modules="$scsi_dh_modules ${i##*/}"
-done
+if test -d $root_dir/lib/modules/$kernel_version/kernel/drivers/scsi/device_handler
+then
+	for i in $(find $root_dir/lib/modules/$kernel_version/kernel/drivers/scsi/device_handler -name "scsi[-_]dh[_-]*.ko")
+	do
+		i=${i%.ko}
+		scsi_dh_modules="$scsi_dh_modules ${i##*/}"
+	done
+fi
 
 save_var scsi_dh_modules
