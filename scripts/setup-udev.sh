bb7ede66a07ef0d9b3d8757283905dc59d4566aa
Olaf Hering <olaf@aepfle.de>
2013-10-17 11:07:07 +0200

    Add support for predictable network interface names (bnc#820407)

--- scripts/setup-udev.sh
+++ scripts/setup-udev.sh
@@ -9,6 +9,7 @@ udev_timeout=30
 
 mkdir -p $tmp_mnt/etc/udev/rules.d
 mkdir -p $tmp_mnt/usr/lib/udev/rules.d
+mkdir -p $tmp_mnt/usr/lib/udev/hwdb.d
 mkdir -p $tmp_mnt/usr/lib/systemd
 mkdir -p $tmp_mnt/lib
 ln -sfn ../usr/lib/udev $tmp_mnt/lib/udev
@@ -78,8 +79,11 @@ for rule in \
     61-msft.rules \
     62-dm_linear.rules \
     64-device-mapper.rules \
+    75-net-description.rules \
     79-kms.rules \
-    80-drivers.rules; do
+    80-drivers.rules \
+    80-net-name-slot.rules \
+    ; do
     if [ -f /usr/lib/udev/rules.d/$rule ]; then
         cp /usr/lib/udev/rules.d/$rule $tmp_mnt/usr/lib/udev/rules.d
     elif [ -f /lib/udev/rules.d/$rule ]; then
@@ -88,5 +92,10 @@ for rule in \
         cp /etc/udev/rules.d/$rule $tmp_mnt/etc/udev/rules.d
     fi
 done
+
+cp -t $tmp_mnt/usr/lib/udev/hwdb.d \
+    /usr/lib/udev/hwdb.d/*pci*.hwdb \
+    /usr/lib/udev/hwdb.d/*acpi*.hwdb \
+    /usr/lib/udev/hwdb.d/*usb*.hwdb
 
 save_var udev_timeout
