79135a393de2c9758d5dc5dd7771bfe351471da3
Hannes Reinecke <hare@suse.de>
2014-01-14 12:25:42 +0100

    Add cciss compat rules to mkinitrd
    We need to include the cciss compability rules to create the
    compat symlinks during boot.
    
    References: bnc#858663
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- scripts/setup-udev.sh
+++ scripts/setup-udev.sh
@@ -21,6 +21,7 @@ for rule in \
     62-dm_linear.rules \
     64-device-mapper.rules \
     64-md-raid.rules \
+    65-cciss-compat.rules \
     79-kms.rules \
     80-drivers.rules; do
     if [ -f /lib/udev/rules.d/$rule ]; then
