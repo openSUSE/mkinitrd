9a77826cb6e4f85706811d386f5afdea140db2b1
Petr Tesarik <ptesarik@suse.com>
2018-11-07 20:45:11 +0100

    Make initrd content private
    With FADUMP active, the initrd may contain sensitive information, such
    as private SSH keys required to save the crash dump to a remote machine.
    Make sure that this information is not readable by ordinary users.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- scripts/setup-done.sh
+++ scripts/setup-done.sh
@@ -13,7 +13,7 @@ fi
 pushd . > /dev/null 2>&1
 cd $tmp_mnt
 find bin sbin -type f -print0 | xargs -0 chmod 0755
-if ! find . ! -name "*~" | cpio -H newc --create | $COMPRESS > $tmp_initrd
+if ! (umask 0077 && find . ! -name "*~" | cpio -H newc --create | $COMPRESS > $tmp_initrd)
 then
     oops 8 "Failed to build initrd"
 fi
