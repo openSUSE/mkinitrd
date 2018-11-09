18c74d5ddbe164a2f0dc96547d42a6c507399530
Martin Wilck <mwilck@suse.com>
2018-11-09 15:50:55 +0100

    make initrd content even more safe (take 2)
    If the target file exists already, cp doesn't change permissions.
    
    The previously attempted "chmod 0600" is an incomplete fix,
    as someone might have an open file descriptor to the file.
    Removing the old file before copying would work, but leave the
    user without a working initrd if the copy fails.
    
    Thus copy to a new mktemp file on the same file system as the target
    (mktemp always creates files with mode 0600), which can then be
    atomically moved to the final path.

--- scripts/setup-done.sh
+++ scripts/setup-done.sh
@@ -22,15 +22,20 @@ then
     oops 8 "Failed to build initrd"
 fi
 popd > /dev/null 2>&1
-# If the target file exists already, cp doesn't change permissions.
-# Even cp -p applies permissions only after copying the contents, leaving
-# a small race window open.
-# We could simply "rm -f" the image, but then in case of failure the user
-# would be left without initrd. Try chmod.
-if [ -e $initrd_image ]; then
-    chmod 0600 $initrd_image || oops 8 "Failed to set initrd permissions"
+
+# Make sure the permissions are safe while copying to final path.
+tmp_initrd_1=$(mktemp "$initrd_image".XXXXXX)
+initrd_ok=no
+if [ -n "$tmp_initrd_1" ]; then
+    if cp -f "$tmp_initrd" "$tmp_initrd_1"; then
+	if mv -fT "$tmp_initrd_1" "$initrd_image"; then
+	    initrd_ok=yes
+	else
+	    rm -f "$tmp_initrd_1"
+	fi
+    fi
 fi
-if ! (umask 0077 && cp -f $tmp_initrd $initrd_image) ; then
+if [ o"$initrd_ok" != oyes ]; then
     oops 8 "Failed to install initrd"
 fi
 rm -rf $tmp_mnt
